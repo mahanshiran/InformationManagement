24dd62fad731056e824ba99ce3750e434d554a38
mahanshiran <mahanshiran@hotmail.com>
2022-09-13 09:08:12 +0800

    Initial Commit

---
+++ Shared/InformationManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  InformationManagementApp.swift
+//  Shared
+//
+//  Created by mahan on 2022/9/13.
+//
+
+import SwiftUI
+
+@main
+struct InformationManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
